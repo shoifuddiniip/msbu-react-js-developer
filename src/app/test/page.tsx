e6d99915164f73ed4f5943e2f6aebd3fb4d40685
Iip Shoifuddin <115620789+shoifuddiniip@users.noreply.github.com>
2025-01-31 07:42:29 +0700

    cahnge image src

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -6,11 +6,6 @@ import getMovies from "../../services/home";
 import NavBar from "../../components/NavBar";
 import Image from "next/image";
 
-const imageLoader = (path: string) => {
-  return `https://image.tmdb.org/t/p/w500/${path}`
-}
-
-
 export default function HomePage() {
   const { data, isLoading, isError } = useQuery({
     queryFn: async () => await getMovies(),
@@ -30,7 +25,13 @@ export default function HomePage() {
             <Col xs={24} sm={12} md={8} lg={6} key={movie.id}>
               <Card
                 hoverable
-                cover={<Image alt={movie.title} src={imageLoader(movie.poster_path)} />}
+                cover={<Image
+                  alt={movie.title} 
+                  src={"https://image.tmdb.org/t/p/w500" + movie.poster_path}
+                  unoptimized
+                  width={500}
+                  height={450}
+                />}
                 style={{ backgroundColor: '#1a1a1a', color: 'white' }}
               >
                 <Card.Meta
