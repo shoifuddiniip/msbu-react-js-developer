0875031617fba7d3bf733da360d183610e7cd548
Iip Shoifuddin <115620789+shoifuddiniip@users.noreply.github.com>
2025-02-02 21:31:32 +0700

    c19

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -46,7 +46,7 @@ const Navbar: React.FC<Props> = (props: Props) => {
     { name: 'Vaksinasi & Imunisasi', icon: ExperimentOutlined, key: 'vi', link: '/vaksinasi-dan-imunisasi' },
     { name: 'Kesehatan Anak', icon: FileDoneOutlined, key: 'ka', link: '/kesehatan-anak' },
     { name: 'Kesehatan Kehamilan', icon: FileDoneOutlined, key: 'kk', link: '/kesehatan-kehamilan' },
-    { name: 'Covid 19', icon: ExperimentOutlined, key: 'c19', link: '' },
+    { name: 'Covid 19', icon: ExperimentOutlined, key: 'c19', link: '/covid-19' },
     { name: 'Cari Layanan Kesehatan', icon: ExperimentOutlined, key: 'cls', link: '' },
     { name: 'Layanan Obat dan Vitamin', icon: DesktopOutlined, key: 'lov', link: '' },
   ].map(
